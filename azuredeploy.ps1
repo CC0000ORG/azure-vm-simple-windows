22fd28cada6033dab1bf0acff357712a78f4e52f
CC0001gmxde <72203217+CC0001gmxde@users.noreply.github.com>
2020-11-20 19:16:56 +0100

    Update azuredeploy.ps1

--- azuredeploy.ps1
+++ azuredeploy.ps1
@@ -14,7 +14,7 @@ $dnsLabelPrefix = Read-Host -Prompt "Enter an unique DNS name for the public IP"
 New-AzResourceGroup -Name $resourceGroupName -Location "$location"
 New-AzResourceGroupDeployment `
     -ResourceGroupName $resourceGroupName `
-    -TemplateUri "https://raw.githubusercontent.com/CC0001MK/azure-vm-simple-windows/main/azuredeploy.ps1" `
+    -TemplateUri "https://raw.githubusercontent.com/CC0001MK/azure-vm-simple-windows/main/azuredeploy.json" `
     -adminUsername $adminUsername `
     -adminPassword $adminPassword `
     -dnsLabelPrefix $dnsLabelPrefix
